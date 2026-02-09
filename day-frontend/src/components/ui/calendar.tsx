38c62cc28eccb54db2ebf0b00703cc3e55b884c6
DosDaiynSol <daiyn.solutions@gmail.com>
2026-02-09 23:40:25 +0500

    fix pr 2

--- day-frontend/src/components/ui/calendar.tsx
+++ day-frontend/src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
 import { cn } from '../../lib/utils'
 
@@ -41,8 +41,18 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ className, orientation }) => {
+          if (orientation === 'left') {
+            return <ChevronLeft className={cn('h-4 w-4', className)} />
+          }
+          if (orientation === 'right') {
+            return <ChevronRight className={cn('h-4 w-4', className)} />
+          }
+          if (orientation === 'up') {
+            return <ChevronUp className={cn('h-4 w-4', className)} />
+          }
+          return <ChevronDown className={cn('h-4 w-4', className)} />
+        },
       }}
       {...props}
     />
